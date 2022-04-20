c0bbcbf383ca4992ca3c80200ea81ec123fbafe9
Shawn Maddock <smaddock@gocirrus.com>
2022-04-20 00:13:42 -0600

    bugfix

--- build.sh
+++ build.sh
@@ -15,10 +15,13 @@ echo "$BUILD" > build/.build_number
 sed \
     --expression="s/~[0-9]+$/~$BUILD/g" \
     --in-place \
+    --regexp-extended \
     payload/DEBIAN/control
 
 # Build package (compressed with xz level 6)
 dpkg-deb --root-owner-group --build ./payload/ ./build
 
 # Wrap archive for better transport
+pushd build
 tar -xzf "bluesky-server_3.0.0alpha~${BUILD}_all.deb.tar.gz" "bluesky-server_3.0.0alpha~${BUILD}_all.deb"
+popd
